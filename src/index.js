ff336f8fc139dce3684768c0ce1db00c4444a2e8
madson7 <almeida.madson44@gmail.com>
2020-01-05 23:09:41 -0300

    go

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
+import DetailsScreen from './pages/DetailsScreen';
+import LoginScreen from './pages/LoginScreen';
 
-import { StyledView, StyledText, StyledButton, StyledButtonText, StyledImage } from './styles';
+import { StyledView, StyledImage, ViewButton } from './styles';
 
-export default class Index extends React.Component {
+class HomeScreen extends React.Component {
+	static navigationOptions = {
+		title: 'Bem Vindos',
+	};
+
 	render() {
 		return (
 			<StyledView>
 				<StyledImage source={require('../assets/Logo.png')} />
-				<StyledText>Bem-vindos</StyledText>
-				<TouchableOpacity>
-					<StyledButton>
-						<StyledButtonText 
-						onPress = {() => this.props.navigation.navigate('Login')} >
-							Login
-							</StyledButtonText>
-					</StyledButton>
-				</TouchableOpacity>
-				<TouchableOpacity>
-					<StyledButton>
-						<StyledButtonText>Registrar</StyledButtonText>
-					</StyledButton>
-				</TouchableOpacity>
+				<ViewButton>
+					<Button
+						title="Login"
+						onPress={() => {
+							this.props.navigation.navigate('Login');
+						}}
+					/>
+				</ViewButton>
+				<ViewButton>
+					<Button
+						title="Registrar"
+						onPress={() => {
+							this.props.navigation.navigate('Login');
+						}}
+					/>
+				</ViewButton>
 			</StyledView>
 		);
+	}
+}
+
+const RootStack = createStackNavigator({
+	Home: HomeScreen,
+	Login: LoginScreen,
+	//Details: DetailsScreen,
+
+	initialRouteName: 'Home',
+});
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+	render() {
+		return <AppContainer />;
 	}
 }
--- src/styles.js
+++ src/styles.js
@@ -4,29 +4,15 @@ export const StyledView = styled.View`
 	background-color: #27ae60;
 	flex: 1;
 	align-items: center;
-	justify-content: center;
 `;
 
 export const StyledImage = styled.Image`
 	width: 150px;
 	height: 150px;
+	margin-top: 40px;
 `;
 
-export const StyledText = styled.Text`
-	color: black;
-	font-size: 28px;
-`;
-
-export const StyledButton = styled.View`
-	margin: 4px;
-	background-color: #ffffff;
-	width: 187.5px;
-	height: 30px;
-	align-items: center;
-	justify-content: center;
-`;
-
-export const StyledButtonText = styled.Text`
-	color: black;
-	font-size: 18px;
+export const ViewButton = styled.View`
+	width: 200px;
+	margin-top: 18px;
 `;
