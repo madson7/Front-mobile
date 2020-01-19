497c7c61089817403b4b5c614c875a2d2bb9e72e
madson7 <almeida.madson44@gmail.com>
2020-01-19 10:01:17 -0300

    erro não detectado: o empacotador não está sendo executado em

--- src/Pages/Login/LoginScreen.js
+++ src/Pages/Login/LoginScreen.js
@@ -1,60 +1,17 @@
 import * as yup from 'yup';
 import { Formik } from 'formik';
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
 
 import React, { Fragment } from 'react';
 
-import { StyledView, StyledView2, ViewButton, FormikView } from './styles';
+import { Container, Container2, StyledView, StyledView2, ViewButton, FormikView, Button } from './styles';
 
 const SignUpScreen = () => (
-	<StyledView>
-		<FormikView>
-		<StyledView2>
-			<Formik
-				initialValues={{ email: '', password: '' }}
-				onSubmit={values => Alert.alert(JSON.stringify(values))}
-				//onSubmit={({ value }, formikBag) => onSubmit(value, formikBag)}
-				validationSchema={yup.object().shape({
-					email: yup
-						.string()
-						.email()
-						.required(),
-					password: yup
-						.string()
-						.min(6)
-						.required(),
-				})}
-			>
-				{({ values, handleChange, errors, setFieldTouched, touched, isValid, handleSubmit }) => (
-					<Fragment>
-						<TextInput
-							value={values.email}
-							onChangeText={handleChange('email')}
-							onBlur={() => setFieldTouched('email')}
-							placeholder="E-mail"
-						/>
-						{touched.email && errors.email && (
-							<Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
-						)}
-						<TextInput
-							value={values.password}
-							onChangeText={handleChange('password')}
-							placeholder="Password"
-							onBlur={() => setFieldTouched('password')}
-							secureTextEntry={true}
-						/>
-						{touched.password && errors.password && (
-							<Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
-						)}
-						<ViewButton>
-							<Button title="Sign In" disabled={!isValid} onPress={handleSubmit} />
-							</ViewButton>
-					</Fragment>
-				)}
-			</Formik>
-			</StyledView2>
-		</FormikView>
-	</StyledView>
+	<Container>
+		<Container2>
+			<Button title="Sign In" />
+		</Container2>
+	</Container>
 );
 
 SignUpScreen.navigationOptions = {
--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -1,15 +1,23 @@
 import styled from 'styled-components/native';
 
-export const StyledView = styled.View`
-	background-color: #333333;
+export const Container = styled.View`
 	flex: 1;
+	background-color: #333333;
+`;
+export const Container2 = styled.View`
+	margin-top: 35px;
+`;
+
+export const Button = styled.Button`
+	margin-top: 5px;
+	margin-left: 20px;
+	margin-right: 20px;
 `;
 
 export const StyledView2 = styled.View`
-	margin-top: 25px;
-	width: 280px;
-	
-	
+	margin-top: 0px;
+	margin-left: 20px;
+	margin-right: 20px;
 `;
 
 export const FormikView = styled.View`
